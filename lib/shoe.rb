6b7b0160b7bac9118ddcadf4635ab50a62424864
Wahome <vinwahome@gmail.com>
2023-05-18 10:54:14 +0300

    Completed Assignment

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,31 @@
 # Make your shoe class here!
+class Shoe 
+    attr_reader :brand
+    attr_accessor :color, :size, :material, :condition
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def initialize(color)
+        @color = color
+    end
+
+    def initialize(size)
+        @size = size
+    end
+
+    def initialize(material)
+        @material = material
+    end
+
+    def initialize(condition)
+        @condition = condition
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+    end
+
+
+end
